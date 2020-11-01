fa8c912e69e48bc7dca92ec9ef320a94c76cfffe
matteopratanamakmur <matteopratanamakmur.Main@gmail.com>
2020-11-01 22:57:45 +0900

    fix

--- main.go
+++ main.go
@@ -38,10 +38,28 @@ func main() {
 	}
 	pe[0].Value(os.Getenv("NEO_PASS"))
 
-	// // click
-	// b := page.FindByID("neo-login-btn")
-	// b.Click()
+	// click
+	b := page.FindByID("neo-login-btn")
+	b.Click()
+	time.Sleep(time.Second * 1)
+
+	// open portfolio page
+	page.Navigate("https://trade.sbineomobile.co.jp/account/portfolio")
+	time.Sleep(time.Second * 5)
+
+	// page size
+	page.Size(300, 1080)
+	time.Sleep(time.Second * 5)
+
+	// scroll
+	for i := 0; i < 10; i++ {
+		err = page.RunScript("scroll(0, document.body.scrollHeight);", nil, nil)
+		if err != nil {
+			log.Fatalf("%s\n", err)
+		}
+		time.Sleep(time.Second * 1)
+	}
 
 	// sleep
-	time.Sleep(time.Second * 10)
+	time.Sleep(time.Second * 30)
 }
