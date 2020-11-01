217dad6d60bf6d27c7e542be204170d8b3feb70e
matteopratanamakmur <matteopratanamakmur.Main@gmail.com>
2020-11-01 21:41:14 +0900

    fix

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/PuerkitoBio/goquery v1.6.0
 	github.com/sclevine/agouti v3.0.0+incompatible
 	github.com/tebeka/selenium v0.9.9 // indirect
+	golang.org/x/tools/gopls v0.5.2 // indirect
 )
--- main.go
+++ main.go
@@ -1,27 +1,46 @@
 package main
 
 import (
-	"fmt"
-	"os"
+	"log"
+	"time"
 
 	"github.com/sclevine/agouti"
 )
 
 func main() {
+	// user / pass
+	const username = "username"
+	const password = "password"
+
 	driver := agouti.ChromeDriver()
 	defer driver.Stop()
 
 	if err := driver.Start(); err != nil {
-		fmt.Fprintf(os.Stderr, "%s\n", err)
-		return
+		log.Fatalf("%s\n", err)
 	}
 
 	page, err := driver.NewPage()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "%s\n", err)
-		return
+		log.Fatalf("%s\n", err)
 	}
 
+	// open neomoba page
 	page.Navigate("https://trade.sbineomobile.co.jp/login")
-	fmt.Println(page.Title())
+
+	// put user info
+	u := page.FindByName(username)
+	ue, err := u.Elements()
+	if err != nil {
+		log.Fatalf("%s\n", err)
+	}
+	ue[0].Value(username)
+	p := page.FindByName(password)
+	pe, err := p.Elements()
+	if err != nil {
+		log.Fatalf("%s\n", err)
+	}
+	pe[0].Value(password)
+
+	// click
+	time.Sleep(time.Second * 1)
 }
