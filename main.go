9955e3cf928b87e845c061b416419d43472ec641
matteopratanamakmur <matteopratanamakmur.Main@gmail.com>
2020-11-02 10:52:15 +0900

    fix

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"time"
@@ -8,18 +9,21 @@ import (
 	"github.com/sclevine/agouti"
 )
 
+func failOnError(err error) {
+	if err != nil {
+		log.Fatal("Error:", err)
+	}
+}
+
 func main() {
 	driver := agouti.ChromeDriver()
 	defer driver.Stop()
 
-	if err := driver.Start(); err != nil {
-		log.Fatalf("%s\n", err)
-	}
+	err := driver.Start()
+	failOnError(err)
 
 	page, err := driver.NewPage()
-	if err != nil {
-		log.Fatalf("%s\n", err)
-	}
+	failOnError(err)
 
 	// open neomoba page
 	page.Navigate("https://trade.sbineomobile.co.jp/login")
@@ -27,15 +31,11 @@ func main() {
 	// put user info
 	u := page.FindByName("username")
 	ue, err := u.Elements()
-	if err != nil {
-		log.Fatalf("%s\n", err)
-	}
+	failOnError(err)
 	ue[0].Value(os.Getenv("NEO_USER"))
 	p := page.FindByClass("input-password")
 	pe, err := p.Elements()
-	if err != nil {
-		log.Fatalf("%s\n", err)
-	}
+	failOnError(err)
 	pe[0].Value(os.Getenv("NEO_PASS"))
 
 	// click
@@ -54,12 +54,14 @@ func main() {
 	// scroll
 	for i := 0; i < 10; i++ {
 		err = page.RunScript("scroll(0, document.body.scrollHeight);", nil, nil)
-		if err != nil {
-			log.Fatalf("%s\n", err)
-		}
+		failOnError(err)
 		time.Sleep(time.Second * 1)
 	}
 
+	// get price
+	plm := page.AllByClass("panels")
+	fmt.Print(plm.All("table"))
+
 	// sleep
-	time.Sleep(time.Second * 30)
+	time.Sleep(time.Second * 60)
 }
